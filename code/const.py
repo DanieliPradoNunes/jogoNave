4cd40583ed7a1557ef491869c3424feb46d77918
DanieliPradoNunes <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>
2026-06-17 17:53:07 -0300

    Interface do  menu

---
+++ code/const.py
@@ -0,0 +1,18 @@
+#c
+C_ORANGE = (255, 165, 0)
+C_BLACK = (0, 0, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+
+#M
+MENU_OPTION = (
+    'NEW GAME 1P',
+    'NEW GAME 2P COPERATIVE',
+    'NEW GAME 2P COMPETITIVE',
+    'SCORES',
+    'EXIT'
+)
+
+#w
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import pygame
+from code.game import Game
 
-print('Setup start')
-pygame.init()
-window = pygame.display.set_mode(size=(960, 540))
-print('Setup end')
-
-print('Loop start')
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+game = Game()
+game.run()
